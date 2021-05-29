882658e114eea8d7dcc33c788e4e156206216240
panda <rautonkar3@gmail.com>
2021-05-29 20:29:11 +0530

    added search input

--- src/components/Links/LinksAction.jsx
+++ src/components/Links/LinksAction.jsx
@@ -3,7 +3,12 @@ import { useState } from "react";
 export default function LinksAction(props) {
   const [newFormActive, setNewFormActive] = useState(true);
   return (
-    <div className="w-100 w-2/4  mx-auto flex justify-end">
+    <div className="w-100 w-2/4  mx-auto flex justify-between">
+      <input
+        type="text"
+        className="w-4/12 p-2 border font-size-1 border-gray-400"
+        placeholder="Search"
+      ></input>
       <button
         className="px-5 py-2 tracking-wider bg-gray-800 text-gray-50 rounded-sm disabled:opacity-50 disabled:cursor-not-allowed"
         disabled={newFormActive}
